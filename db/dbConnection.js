62623fd7bcb0267ab688964aac80eef6a3ccbb18
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2024-10-17 11:21:22 +0000

    code added

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -1,17 +1,34 @@
 import mongoose from "mongoose";
 
-const connectDB = async ()=>{
-    try {
-        const connectionInstance = await mongoose.connect(`mongodb://localhost:27017`);
-        console.log("Mongodb Connected");
-    
-    }
-    catch(error){
-        console.log("MongoDB Connection Failed: ",error);
-        process.exit(1); // node js error syntax
-    }
+const connectDB = async () => {
+    await mongoose.connect("mongodb://localhost:27017/HMIS")
+    .then(() => {
+        console.log("Connected to database successfully!");
+    })
+    .catch((err) => {
+        console.log(`Error connecting to database: ${err}`);
+    })
 }
 
-export default connectDB;
+const createCollection = async (collectionName) => {
+    const database = await mongoose.connection.useDb();
+    await database.createCollection(collectionName)
+    .then(() => {
+        console.log(`Collection named ${collectionName} created!`);
+    })
+    .catch((err) => {
+        console.log(`Error creating collection: ${err}`);
+    })
+}
 
+const checkForCollection = async (collectionName) => {
+    const database = await mongoose.connection.useDb();
+    const dbCollections = await database.collections();
+    const requiredCollection = dbCollections.filter((collection) => collection.name == collectionName);
+    if (!requiredCollection) {
+        createCollection(collectionName);
+    }
+    console.log("Collection already exists!");
+}
 
+export default {connectDB, createCollection, checkForCollection};
